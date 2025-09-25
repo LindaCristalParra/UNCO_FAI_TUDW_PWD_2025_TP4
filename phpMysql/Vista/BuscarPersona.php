660ab4232d144a1f5354117f8c8387b1e73548a4
Andrea Crespillo <andrea.crespillo@est.fi.uncoma.edu.ar>
2025-09-25 16:04:29 -0300

    Cambiar BuscarPersona de HTML a PHP

--- phpMysql/Vista/BuscarPersona.php
+++ phpMysql/Vista/BuscarPersona.php
@@ -16,20 +16,20 @@ include __DIR__ . '/estructura/header.php';
         </form>
       </div>
     </div>
-  </main>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-  <script>
-  (function(){
-    'use strict';
-    const form = document.getElementById('formBuscarPersona');
-    form.addEventListener('submit', function (event) {
-      if (!form.checkValidity()) {
-        event.preventDefault();
-        event.stopPropagation();
-      }
-      form.classList.add('was-validated');
-    }, false);
-  })();
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+    (function(){
+      'use strict';
+      const form = document.getElementById('formBuscarPersona');
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+      })();
   </script>
 <?php include __DIR__ . '/estructura/footer.php'; ?>
 
